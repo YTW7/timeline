568cdc03d546fd71b892632b94cb07e98814aae9
YTW7 <91966435+YTW7@users.noreply.github.com>
2023-05-25 19:50:02 +0000

    added files

---
+++ time/src/pages/index.js
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import timelineData from '../data/timelinedata'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const TimelineItem = ({ data }) => (
+    <div className="timeline-item">
+        <div className="timeline-item-content">
+            <span className="tag" style={{ background: data.category.color }}>
+                {data.category.tag}
+            </span>
+            <time>{data.date}</time>
+            <p>{data.text}</p>
+            {data.link && (
+                <a
+                    href={data.link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {data.link.text}
+                </a>
+            )}
+            <span className="circle" />
+        </div>
+    </div>
+);
+const Timeline = () =>
+    timelineData.length > 0 && (
+        <div className="timeline-container">
+            {timelineData.map((data, idx) => (
+                <TimelineItem data={data} key={idx} />
+            ))}
+        </div>
+    );
+  return (
+    
+
+<>
+<Timeline/>
+</>
+
+
+  )
+}
